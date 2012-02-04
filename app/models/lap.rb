eb2b8a210a8ba3202361887412b6dedf0c947cea
John Dowd <dowdjj@gmail.com>
2012-02-04 07:50:52 -0800

    added best and avg lap times

--- app/models/lap.rb
+++ app/models/lap.rb
@@ -14,6 +14,20 @@ class Lap < ActiveRecord::Base
   belongs_to :session
   
   def self.best_session_time(session)
-    where(:session_id => session).map(&:tenths).min
+    best = where(:session_id => session).map(&:tenths).min
+    format_lap_time(best)
   end
+  
+  def self.avg_session_time(session)
+    total = where(:session_id => session.id).average('tenths')
+    format_lap_time(total)
+  end
+  
+  
+  private
+    def self.format_lap_time(tenths)
+      decimal = (tenths % 10).to_i
+      sec = (tenths / 10).floor
+      "#{sec}.#{decimal}"
+    end
 end
